81a63deaac5cb2b8fe0aeafb76e5e89b36a7fdca
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-16 13:48:28 -0400

    finished lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+# Make your shoe class here!  1) Shoe ::new gets initialized with a brand
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
